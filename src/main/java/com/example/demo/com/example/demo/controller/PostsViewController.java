ce08d04c83679f77e5882cfe869e2b240c5eab5e
Kazmin Mihail <mihail.kazmin.04@mail.ru>
2024-02-10 23:26:34 +0500

    Page Моё супер приложение

--- src/main/java/com/example/demo/com/example/demo/controller/PostsViewController.java
+++ src/main/java/com/example/demo/com/example/demo/controller/PostsViewController.java
@@ -1,6 +1,8 @@
 package com.example.demo.com.example.demo.controller;
 
 
+import com.example.demo.com.example.demo.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -8,7 +10,8 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class PostsViewController
 {
-
+    @Autowired
+    PostService postsService;
     @ResponseBody
     @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
     public String single(@PathVariable("id") Long id) {
@@ -17,8 +20,10 @@ public class PostsViewController
 
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String list(Model model) {
+    public String list(Model model)
+    {
         model.addAttribute("appName", "Моё супер приложение");
+        model.addAttribute("posts", postsService.listAllPosts());
         return "list";
     }
 }
